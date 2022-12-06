dd5fab43e21a6f62fe49da682a063fb7bd5d3e36
xu.zhishun <774590465@qq.com>
2022-12-06 11:50:06 +0800

    demo级别 server,client

---
+++ connect/src/main/java/cn/rookiex/client/Client.java
@@ -0,0 +1,34 @@
+package cn.rookiex.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author rookieX 2022/12/5
+ */
+public class Client {
+    public static ChannelFuture newChannel(String ip, int port) throws Exception {
+        // Configure the client.
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap b = new Bootstrap();
+        b.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        p.addLast(new LoggingHandler(LogLevel.INFO));
+                        p.addLast(new EchoClientHandler());
+                    }
+                });
+
+        // Start the client.
+        return b.connect(ip, port).sync();
+    }
+}
