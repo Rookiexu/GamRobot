ecbe03f38001bf3e88fe2e1b5813f359495ca585
xu.zhishun <774590465@qq.com>
2022-12-14 10:12:46 +0800

    模组顺序执行状态流程优化

--- robot/src/main/java/cn/rookiex/module/stage/OrderStage.java
+++ robot/src/main/java/cn/rookiex/module/stage/OrderStage.java
@@ -5,12 +5,14 @@ import cn.rookiex.module.Module;
 import cn.rookiex.module.ModuleManager;
 import cn.rookiex.robot.Robot;
 import cn.rookiex.robot.RobotContext;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.List;
 
 /**
  * @author rookieX 2022/12/12
  */
+@Log4j2
 public class OrderStage implements ModuleStage {
 
     ModuleStage stage = new RandomStage();
@@ -53,20 +55,33 @@ public class OrderStage implements ModuleStage {
     @Override
     public void initMod(RobotContext robotContext) {
         Robot robot = robotContext.getRobot();
-        robot.setCurModStage(Module.ORDER);
 
         ModuleManager moduleManager = robot.getModuleManager();
+
         List<Module> orderModule = moduleManager.getOrderModule();
         int curModIdx = robot.getCurModIdx();
-        if (orderModule.size() > curModIdx) {
-            Module module = orderModule.get(curModIdx);
-            module.initRunEvent(robotContext);
-        }
+        Module module = orderModule.get(curModIdx);
+        module.initRunEvent(robotContext);
+    }
+
+    @Override
+    public void initStage(RobotContext robotContext) {
+        Robot robot = robotContext.getRobot();
+        robot.setCurModStage(Module.ORDER);
+
+        robot.setCurModIdx(0);
+        initMod(robotContext);
     }
 
     @Override
     public ModuleStage nextStage(RobotContext robotContext) {
         return stage;
+    }
+
+    @Override
+    public void toNextMod(RobotContext robotContext) {
+        Robot robot = robotContext.getRobot();
+        robot.setCurModIdx(robot.getCurModIdx() + 1);
     }
 
     @Override
