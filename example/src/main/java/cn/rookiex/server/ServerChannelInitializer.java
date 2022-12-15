e40a3bc5664e4799c7a59e4be008a450eaa6bc1e
xu.zhishun <774590465@qq.com>
2022-12-15 10:02:59 +0800

    echo Server

---
+++ example/src/main/java/cn/rookiex/server/ServerChannelInitializer.java
@@ -0,0 +1,26 @@
+package cn.rookiex.server;
+
+import cn.rookiex.coon.ClientHandler;
+import cn.rookiex.coon.MsgDecoder;
+import cn.rookiex.coon.MsgEncoder;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author rookieX 2022/12/14
+ */
+public class ServerChannelInitializer extends ChannelInitializer {
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new LoggingHandler(LogLevel.INFO));
+        p.addLast(new LengthFieldBasedFrameDecoder(1024 * 512, 0, 4, 0, 4));
+        p.addLast(new MsgDecoder());
+        p.addLast(new ServerHandler());
+        p.addLast(new MsgEncoder());
+    }
+}
---
+++ example/src/main/java/cn/rookiex/server/ServerHandler.java
@@ -0,0 +1,42 @@
+package cn.rookiex.server;
+
+import cn.hutool.core.util.RandomUtil;
+import cn.rookiex.RobotServer;
+import cn.rookiex.coon.SimpleMessage;
+import cn.rookiex.manager.RobotManager;
+import cn.rookiex.robot.Robot;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author rookieX 2022/12/14
+ */
+@Log4j2
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+       //todo 连接活跃处理
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        SimpleMessage message = (SimpleMessage) msg;
+        Thread.sleep(RandomUtil.randomInt(5));
+        ctx.channel().writeAndFlush(message);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        // todo 连接断开处理
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+}
